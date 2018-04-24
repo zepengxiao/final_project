d6bdd5c42efe75e314291d7f8e768e7f05521b35
zepengxiao <zepengx2@illinois.edu>
2018-04-24 16:04:07 -0500

    final project UI

---
+++ app/src/main/java/edu/illinois/cs/cs125/fortunetellingapp/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.illinois.cs.cs125.fortunetellingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
